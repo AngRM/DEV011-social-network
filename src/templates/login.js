5de8036811902141d08888cae8fc16023dd7bcfe
Christal <ceiguedezq@email.com>
2023-11-10 12:05:49 -0300

    errores de tipografia corregidos y arreglado el buttonreturn de login

--- src/templates/login.js
+++ src/templates/login.js
@@ -1,58 +1,59 @@
-import { loginUser } from "../lib/auth";
+import { loginUser } from '../lib/auth';
 
 // file login.js
 function login(navigateTo) {
-  const section = document.createElement("section");
-  section.classList.add("login-background");
-  const whiteBackground = document.createElement("div");
-  whiteBackground.classList.add("white-background"); // Agrega una clase al div
-  whiteBackground.style.zIndex = "0"; // Asegura que el fondo blanco esté por encima del fondo colorido
-  const title = document.createElement("h2");
-  const subTitle = document.createElement("h3");
-  const buttonReturn = document.createElement("button");
-  const form = document.createElement("form");
-  const inputEmail = document.createElement("input");
-  const inputPass = document.createElement("input");
-  const buttonLogin = document.createElement("button");
-  buttonLogin.classList.add("buttonlogin");
-  const forgetPass = document.createElement("p");
-  const sectionNewUser = document.createElement("section");
-  const newUser = document.createElement("p");
-  newUser.classList.add("inline-text"); // Agrega una clase a los elementos de texto
-  const buttonNewUser = document.createElement("button");
-  buttonNewUser.classList.add("create-account-button");
-  const headerTextContainer = document.createElement("div");
-  const headerText = document.createElement("div");
-  headerText.textContent = "El verdadero sabor de nuestra tierra";
-  headerText.classList.add("header-text"); // Agrega una clase al div
+  const section = document.createElement('section');
+  section.classList.add('login-background');
+  const whiteBackground = document.createElement('div');
+  whiteBackground.classList.add('white-background'); // Agrega una clase al div
+  whiteBackground.style.zIndex = '0'; // Asegura que el fondo blanco esté por encima del fondo colorido
+  const title = document.createElement('h2');
+  const subTitle = document.createElement('h3');
+  const buttonReturn = document.createElement('button');
+  buttonReturn.classList.add('buttonReturn');
+  const form = document.createElement('form');
+  const inputEmail = document.createElement('input');
+  const inputPass = document.createElement('input');
+  const buttonLogin = document.createElement('button');
+  buttonLogin.classList.add('buttonlogin');
+  const forgetPass = document.createElement('p');
+  const sectionNewUser = document.createElement('section');
+  const newUser = document.createElement('p');
+  newUser.classList.add('inline-text'); // Agrega una clase a los elementos de texto
+  const buttonNewUser = document.createElement('button');
+  buttonNewUser.classList.add('create-account-button');
+  const headerTextContainer = document.createElement('div');
+  const headerText = document.createElement('div');
+  headerText.textContent = 'El verdadero sabor de nuestra tierra';
+  headerText.classList.add('header-text'); // Agrega una clase al div
   headerTextContainer.appendChild(headerText);
 
-  inputEmail.placeholder = "Ingresa correo";
-  inputPass.placeholder = "Contraseña";
+  inputEmail.placeholder = 'Ingresa correo';
+  inputPass.placeholder = 'Contraseña';
 
-  title.textContent = "Acceso";
-  subTitle.textContent = "Inicia sesión para continuar";
-  buttonLogin.textContent = "Iniciar sesión";
-  forgetPass.textContent = "¿Olvidaste tu contraseña?";
-  //Recordar agregarle funcionalidad al boton de olvido y crear pagina correspondiente????///
-  newUser.textContent = "¿Aún no tienes cuenta?";
+  title.textContent = 'Acceso';
+  subTitle.textContent = 'Inicia sesión para continuar';
+  buttonLogin.textContent = 'Iniciar sesión';
+  forgetPass.textContent = '¿Olvidaste tu contraseña?';
+  // Recordar agregarle funcionalidad al boton de olvido y crear pagina correspondiente????///
+  newUser.textContent = '¿Aún no tienes cuenta?';
 
-  buttonReturn.textContent = "Atrás";
-  buttonNewUser.textContent = "Create una nueva";
+  buttonReturn.textContent = 'Atrás';
+  buttonNewUser.textContent = 'Create una nueva';
 
-  buttonReturn.addEventListener("click", () => {
-    navigateTo("/");
+  buttonReturn.addEventListener('click', () => {
+    navigateTo('/');
   });
-  buttonNewUser.addEventListener("click", () => navigateTo("/register"));
-  buttonLogin.addEventListener("click", (event) => {
+  buttonNewUser.addEventListener('click', () => navigateTo('/register'));
+  buttonLogin.addEventListener('click', (event) => {
     event.preventDefault();
-    console.log("clickkkkkk");
+    console.log('clickkkkkk');
     loginUser(inputEmail.value, inputPass.value)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
         console.log(user);
-        navigateTo("/wall");
+        navigateTo('/wall');
         // ...
       })
       .catch((error) => {
@@ -65,14 +66,7 @@ function login(navigateTo) {
   section.appendChild(whiteBackground);
   form.append(inputEmail, inputPass, forgetPass, buttonLogin);
   sectionNewUser.append(newUser, buttonNewUser);
-  section.append(
-    headerTextContainer,
-    title,
-    subTitle,
-    form,
-    buttonReturn,
-    sectionNewUser
-  );
+  section.append(headerTextContainer, title, subTitle, form, buttonReturn, sectionNewUser);
 
   return section;
 }
